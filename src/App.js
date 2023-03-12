b628e5aa4dd3b5832f14093e522ac8dc78d4955d
Akash-Tandale001 <ak.tandale123@gmail.com>
2023-03-12 18:13:26 +0530

    Layout is ready

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import { Suspense } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Layout from './Components/Layout/Layout';
+import Home from './Pages/Home/Home';
+import { BasicRoutesConfig } from './Routes/Router';
+function App() {  
   return (
     <>
-    
+    <Suspense fallback={<div>Loding....!</div>}>
+      <Routes>
+      <Route element={true ? <Layout/> : <Home/>}>
+        {BasicRoutesConfig.map((route, key) => {
+          return route ? <Route key={route.name} {...route} /> : null;
+        })}   
+        </Route>            
+      </Routes>
+    </Suspense>
     
     </>
   );
