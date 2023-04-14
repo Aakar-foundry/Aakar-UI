4baf070bddcaa12211fde2c629513733f6f661f5
Akash-Tandale001 <ak.tandale123@gmail.com>
2023-04-14 11:20:32 +0530

    UI update

--- src/App.js
+++ src/App.js
@@ -11,24 +11,24 @@ import Register from './Pages/Register/Register';
 import ForgotPassword from './Pages/ForgotPassword/ForgotPassword';
 
 
-function App() {  
+function App() {
   return (
     <>
-    <Suspense fallback={<div>Loding....!</div>}>
-      <Routes>
+      <Suspense fallback={<div>Loding....!</div>}>
+        <Routes>
 
-        {/* changes made for react element */}
-      <Route element={true ? <Layout/> : <Home/>}>
+          {/* changes made for react element */}
+          <Route element={true ? <Layout /> : <Home />}>
 
 
-      {/* <Route element={true ? <ForgotPassword/> : <Home/>}> */}
-        {BasicRoutesConfig.map((route, key) => {
-          return route ? <Route key={route.name} {...route} /> : null;
-        })}   
-        </Route>            
-      </Routes>
-    </Suspense>
-    
+            {/* <Route element={true ? <ForgotPassword/> : <Home/>}> */}
+            {BasicRoutesConfig.map((route, key) => {
+              return route ? <Route key={route.name} {...route} /> : null;
+            })}
+          </Route>
+        </Routes>
+      </Suspense>
+
     </>
   );
 }
