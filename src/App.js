41da60fd5e4937e94757af88b75992f420a361ef
Tejasvi <tejasvi.22111066@viit.ac.in>
2023-04-22 16:23:21 +0530

    Dashboard

--- src/App.js
+++ src/App.js
@@ -1,36 +1,44 @@
-
 import { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Layout from './Components/Layout/Layout';
 import Home from './Pages/Home/Home';
 import { BasicRoutesConfig } from './Routes/Router';
+import Employee from './Pages/Employee/Employee';
+import Admin from './Pages/Admin/Admin';
+import Quality from './Pages/Quality/Quality';
+import Machinery from './Pages/Machinery/Machinery';
+import Marketing from './Pages/Marketing/Marketing';
+import Foundary from './Pages/Foundary/Foundary';
+import Designing from './Pages/Designing/Designing';
+import Management from './Pages/Management/Management';
+import Packaging_Logistic from './Pages/Packaging_Logistic/Packaging_Logistic';
 
-// imporoved
-import Login from './Pages/Login/Login';
-import Register from './Pages/Register/Register';
-import ForgotPassword from './Pages/ForgotPassword/ForgotPassword';
-
-
-function App() {
+function App() {  
   return (
     <>
-      <Suspense fallback={<div>Loding....!</div>}>
-        <Routes>
-
-          {/* changes made for react element */}
-          <Route element={true ? <Layout /> : <Home />}>
-
-
-            {/* <Route element={true ? <ForgotPassword/> : <Home/>}> */}
-            {BasicRoutesConfig.map((route, key) => {
-              return route ? <Route key={route.name} {...route} /> : null;
-            })}
-          </Route>
-        </Routes>
-      </Suspense>
-
+    <Suspense fallback={<div>Loding....!</div>}>
+      <Routes>
+      <Route element={true ? <Layout/> : <Home/>}>
+        {BasicRoutesConfig.map((route, key) => {
+          return route ? <Route key={route.name} {...route} /> : null;
+        })}   
+        <Route path="Employee" element={<Employee />} />
+        <Route path="Admin" element={<Admin />} />
+        <Route path="Quality" element={<Quality />} />
+        <Route path="Machinery" element={<Machinery />} />
+        <Route path="Marketing" element={<Marketing />} />
+        <Route path="Foundary" element={<Foundary />} />
+        <Route path="Designing" element={<Designing />} />
+        <Route path="Management" element={<Management />} />
+        <Route path="Packaging_Logistic" element={<Packaging_Logistic />} />
+        </Route>           
+      </Routes>
+    </Suspense>
     </>
   );
 }
 
 export default App;
+
+
+
